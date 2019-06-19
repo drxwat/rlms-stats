ac90f00124eae82801dd1aa3599f5f3a1b44b84d
drxwat <drxwat@gmail.com>
2019-06-19 22:27:31 +0300

    batch data import

--- elem-occ-panel-persons.R
+++ elem-occ-panel-persons.R
@@ -18,8 +18,27 @@ data_sources = dir_info(data_root, recurse = TRUE) %>%
   arrange(wave_num) %>%
   ungroup()
 
-data_sources
+waves_meta = list(
+  list(n = 5, quo(a_born_y), quo(aj10), quo(aj1), quo(aj2cod08), quo(ah5), quo(aj38)),
+  list(n = 6, quo(b_born_y), quo(bj10), quo(bj1), quo(bj2cod08), quo(bh5), quo(bj8)),
+  list(n = 26, quo(vh6), quo(vj13.2), quo(vj1), quo(vj2cod08), quo(vh5), quo(vj8))
+)
 
-# r05iall26c = read_sav("data/Полная выборка 09.09.2018/5-я волна/ИНДИВИДЫ/r05iall26c.sav")
-# data = prepareData(r05iall26c, 5, a_born_y, aj10, aj1, aj2cod08, ah5)
-
+for (wave_meta in waves_meta) {
+  path = (data_sources %>% filter(wave_num == wave_meta$n))$path
+  tmpData = prepareData(
+    read_sav(path), wave_meta$n, 
+    !!wave_meta[[2]], !!wave_meta[[3]], !!wave_meta[[4]], !!wave_meta[[5]], 
+    !!wave_meta[[6]], !!wave_meta[[7]]
+    )
+  attributes(tmpData$idind) = NULL
+  if (exists("data", inherits = FALSE)) {
+    tmpData = tmpData %>% select(-c('gender', 'age'))
+    colnames(tmpData) = c("idind", paste(colnames(tmpData)[-1], wave_meta$n, sep = "_"))
+    data = full_join(data, tmpData, by = 'idind')
+  } else {
+    data = tmpData
+  }
+}
+# remove(tmpData, wave_meta, data_sources, waves_meta, data_root, path, pattern)
+View(data)
--- helpers.R
+++ helpers.R
@@ -9,26 +9,30 @@ replaceNaWithNamedFactor = function(factor_col, name) {
   return(new_factor_col)
 }
 
-# Birth year, salary, has job, job cod, gender, birthplace, nationality, industry
-prepareData = function(data, wave, born_col, salary_col, has_job_col, job_code_col, gender_col) {
+# Birth year, salary, has job, job cod, gender, birthplace, 30 day working hours
+prepareData = function(data, wave, born_col, salary_col, has_job_col, job_code_col, gender_col, whours_col) {
   wave_year = 1999 - wave
   born_col = enquo(born_col)
   salary_col = enquo(salary_col)
   has_job_col = enquo(has_job_col)
   job_code_col = enquo(job_code_col)
   gender_col = enquo(gender_col)
+  whours_col = enquo(whours_col)
   
   data %>% 
     mutate(
       salary_int = as.integer(!! salary_col),
-      has_job_int = as.integer(!! has_job_col)
+      has_job_int = as.integer(!! has_job_col),
+      whours_int = as.integer( !! whours_col)
     ) %>% 
     transmute(
+      idind = idind,
       age = wave_year - !! born_col,
       salary = ifelse(salary_int == 99999997 | salary_int == 99999998 | salary_int == 99999999, NA, salary_int),
       has_job = ifelse(has_job_int == 1 | has_job_int == 2 | has_job_int == 3 | has_job_int == 4, TRUE, FALSE),
       isco08code = as.character(!! job_code_col),
       isco08major = substring(isco08code, 1, 1),
-      gender = factor(!! gender_col, levels = c(1, 2), c('Male', 'Female'))
+      gender = factor(!! gender_col, levels = c(1, 2), c('Male', 'Female')),
+      whours = ifelse(whours_int == 99999997 | whours_int == 99999998 | whours_int == 99999999, NA, whours_int)
     )
 }
